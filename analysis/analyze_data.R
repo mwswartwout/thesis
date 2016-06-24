e04884d1287e03ab01d456a0eb80b85aae761ca1
Matthew Swartwout <mwswartwout@gmail.com>
2016-06-24 13:10:47 -0400

    Delete old reports before regeneration

--- analysis/analyze_data.R
+++ analysis/analyze_data.R
@@ -1,5 +1,9 @@
 data_dir <- "/home/matt/thesis/experiment_data"
 dirs <- list.dirs(path=data_dir)
+report_dir <- paste0(data_dir, "/reports")
+
+# First let's delete any previous reports, as having those around seems to cause errors
+unlink(paste(report_dir, "*", sep="/"), recursive=TRUE)
 
 for (directory in 2:length(dirs)){
     experiment_name = substr(dirs[directory], nchar(data_dir)+2, nchar(dirs[directory]))
@@ -14,10 +18,10 @@ for (directory in 2:length(dirs)){
                 robot_number <- substr(files[file], 10, 11)
             }
 
-            rmarkdown::render("robot.Rmd", params=list(experiment=experiment_name, robot=robot_number), output_file=paste0("turtlebot_", robot_number, ".pdf"), output_dir=paste0(data_dir, "/reports/", experiment_name))
+            rmarkdown::render("robot.Rmd", params=list(experiment=experiment_name, robot=robot_number), output_file=paste0("turtlebot_", robot_number, ".pdf"), output_dir=paste(report_dir, experiment_name, sep="/"))
         }
 
-        rmarkdown::render('experiment.Rmd', params=list(experiment=experiment_name, robots=length(files)), output_file=paste0(experiment_name, ".pdf"), output_dir=paste0(data_dir, "/reports/", experiment_name))
+        rmarkdown::render('experiment.Rmd', params=list(experiment=experiment_name, robots=length(files)), output_file=paste0(experiment_name, ".pdf"), output_dir=paste(report_dir, experiment_name, sep="/"))
     }
 
 
