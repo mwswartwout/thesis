712381c42026462e5a2ab73a30e481899e08d033
Matthew Swartwout <mwswartwout@gmail.com>
2016-06-29 13:28:06 -0400

    Removes saving of covariances, changes writing so that all positions are recorded in the map frame

--- ros_ws/src/cwru_turtlebot/src/cwru_turtlebot/sensor_record.py
+++ ros_ws/src/cwru_turtlebot/src/cwru_turtlebot/sensor_record.py
@@ -6,7 +6,10 @@ import os
 import errno
 from nav_msgs.msg import Odometry
 from std_msgs.msg import UInt64
+from geometry_msgs.msg import PoseWithCovarianceStamped
 
+initial_x = None
+initial_y = None
 continuous_data = None
 discrete_data = None
 gazebo_data = None
@@ -16,34 +19,56 @@ namespace = None
 prefix = None
 
 
+def initial_position_callback(position):
+    # Record the initial position of the robot so that we can convert the odometry values from the robot's odom frame
+    # to the map frame
+    global initial_x
+    global initial_y
+    initial_x = position.pose.pose.position.x
+    initial_y = position.pose.pose.position.y
+
+
 def continuous_odom_callback(new_odom):
     global continuous_data
+    global initial_x
+    global initial_y
 
-    pose_x = new_odom.pose.pose.position.x
-    pose_y = new_odom.pose.pose.position.y
-    pose_covariance = new_odom.pose.covariance
-    twist_covariance = new_odom.twist.covariance
-    continuous_data = [pose_x, pose_y, (''.join(str(pose_covariance))).split(","), (''.join(str(twist_covariance))).split(",")]
+    if None not in (initial_x, initial_y):
+        # Must add in the initial pose values to convert from odom frame to map frame
+        pose_x = new_odom.pose.pose.position.x + initial_x
+        pose_y = new_odom.pose.pose.position.y + initial_y
+        # pose_covariance = new_odom.pose.covariance
+        # twist_covariance = new_odom.twist.covariance
+        # TODO see if we can split covariance values into their own items
+        continuous_data = [pose_x, pose_y]#, (''.join(str(pose_covariance))).split(","), (''.join(str(twist_covariance))).split(",")]
 
 
 def discrete_odom_callback(new_odom):
     global discrete_data
+    global initial_x
+    global initial_y
 
-    pose_x = new_odom.pose.pose.position.x
-    pose_y = new_odom.pose.pose.position.y
-    pose_covariance = new_odom.pose.covariance
-    twist_covariance = new_odom.twist.covariance
-    discrete_data = [pose_x, pose_y, (''.join(str(pose_covariance))).split(","), (''.join(str(twist_covariance))).split(",")]
+    if None not in (initial_x, initial_y):
+        # Measurement is already in map frame so no need to convert by adding initial position
+        pose_x = new_odom.pose.pose.position.x
+        pose_y = new_odom.pose.pose.position.y
+        # pose_covariance = new_odom.pose.covariance
+        # twist_covariance = new_odom.twist.covariance
+        discrete_data = [pose_x, pose_y]#, (''.join(str(pose_covariance))).split(","), (''.join(str(twist_covariance))).split(",")]
 
 
 def gazebo_odom_callback(new_odom):
     global gazebo_data
+    global initial_x
+    global initial_y
 
-    pose_x = new_odom.pose.pose.position.x
-    pose_y = new_odom.pose.pose.position.y
-    pose_covariance = new_odom.pose.covariance
-    twist_covariance = new_odom.twist.covariance
-    gazebo_data = [pose_x, pose_y, (''.join(str(pose_covariance))).split(","), (''.join(str(twist_covariance))).split(",")]
+    if None not in (initial_x, initial_y):
+        # Must add in the initial pose values to convert from odom frame to map frame
+        pose_x = new_odom.pose.pose.position.x + initial_x
+        pose_y = new_odom.pose.pose.position.y + initial_y
+        # pose_covariance = new_odom.pose.covariance
+        # twist_covariance = new_odom.twist.covariance
+        gazebo_data = [pose_x, pose_y]#, (''.join(str(pose_covariance))).split(","), (''.join(str(twist_covariance))).split(",")]
 
 
 def external_pose_count_callback(count):
@@ -52,11 +77,13 @@ def external_pose_count_callback(count):
 
 
 def make_sure_path_exists(path):
+    global namespace
+
     try:
         os.makedirs(path)
-    except OSError as exception:
-        if exception.errno != errno.EEXIST:
-            raise
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            rospy.logwarn(namespace + ': Error when checking file path - ' + e.message)
 
 
 def write_headers():
@@ -70,17 +97,17 @@ def write_headers():
         filename = prefix + namespace + '_continuous_odometry_filtered.csv'
         with open(filename, 'w+') as pose_file:
             writer = csv.writer(pose_file)
-            writer.writerow(['x_position', 'y_position', 'pose_covariance', 'twist_covariance'])
+            writer.writerow(['x_position', 'y_position'])  # , 'pose_covariance', 'twist_covariance'])
 
         filename = prefix + namespace + '_discrete_odometry_filtered.csv'
         with open(filename, 'w+') as pose_file:
             writer = csv.writer(pose_file)
-            writer.writerow(['x_position', 'y_position', 'pose_covariance', 'twist_covariance'])
+            writer.writerow(['x_position', 'y_position'])  # , 'pose_covariance', 'twist_covariance'])
 
         filename = prefix + namespace + '_gazebo_odometry_filtered.csv'
         with open(filename, 'w+') as pose_file:
             writer = csv.writer(pose_file)
-            writer.writerow(['x_position', 'y_position', 'pose_covariance', 'twist_covariance'])
+            writer.writerow(['x_position', 'y_position'])  # , 'pose_covariance', 'twist_covariance'])
 
         filename = prefix + namespace + '_external_pose_count.csv'
         with open(filename, 'w+') as count_file:
@@ -128,6 +155,7 @@ def main():
     global prefix
     prefix = rospy.get_param('/save_file_prefix')
 
+    initial_position_subscriber = rospy.Subscriber('initial_position', PoseWithCovarianceStamped, initial_position_callback)
     continuous_odom_subscriber = rospy.Subscriber('odometry/filtered_continuous', Odometry, continuous_odom_callback)
     discrete_odom_subscriber = rospy.Subscriber('odometry/filtered_discrete', Odometry, discrete_odom_callback)
     gazebo_odom_subscriber = rospy.Subscriber('odom', Odometry, gazebo_odom_callback)
